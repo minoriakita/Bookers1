f1765eaf4622e88215b7eb658d4bc945a224dcba
minoriakita <minori.akita@gmail.com>
2022-04-08 16:26:10 +0000

    [Update] 3-12 保存機能追加途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,25 @@
 class BooksController < ApplicationController
-  def top
+  def new
+    @book = Book.new
+  end
   
+  def create
+    book = Book.new(book_params)
+    list.save
+    #render 
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title :body)
   end
 end
