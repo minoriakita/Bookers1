2325762c62bb84c79bd27dd129ef0b89964390b9
minoriakita <minori.akita@gmail.com>
2022-04-10 16:20:47 +0000

    [Finish] 3-12

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'top' => 'homes#top'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show', as: 'bo'
-  get 'books/edit'
   
+  resources :books
+  get "/" => "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
